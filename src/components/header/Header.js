dedadfa1ae3f6b1c9cd01b192aa77b5ced25f161
Spencer Knight <spencer.knightusn@gmail.com>
2021-10-29 10:09:59 -0700

    header boilerplate setup, ham menu icon and youtube icon correctly placed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,15 @@
 import './header.css';
+import youtubeIcon from '../../assets/youtubeIcon.png';
 
 export default function Header() {
-  return <header>header</header>;
+  return (
+    <header>
+      <div id='leftHeader'>
+        <i id='hamburgerMenuIcon' class='fas fa-bars'></i>
+        <div>
+          <img id='youtubeIcon' src={youtubeIcon} alt='youtube icon'></img>
+        </div>
+      </div>
+    </header>
+  );
 }
