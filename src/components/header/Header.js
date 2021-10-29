f35037cc07335c76290aa15cbe5ef918f5c6fd5a
Spencer Knight <spencer.knightusn@gmail.com>
2021-10-29 10:51:33 -0700

    icon text complete

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,15 @@
 import './header.css';
 import youtubeIcon from '../../assets/youtubeIcon.png';
+import IconText from './IconText';
 
 export default function Header() {
   return (
     <header>
       <div id='leftHeader'>
         <i id='hamburgerMenuIcon' class='fas fa-bars'></i>
-        <div>
+        <div id='youtubeIconContainer'>
           <img id='youtubeIcon' src={youtubeIcon} alt='youtube icon'></img>
+          <IconText />
         </div>
       </div>
     </header>
