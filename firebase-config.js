02d38188343606fe456e81977d9e25ef8215e225
Spencer Knight <spencer.knightusn@gmail.com>
2021-10-30 09:45:50 -0700

    added firebase config

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDXVky7qYwvfz5KOXwBvNpXr810hRfgVAc',
+  authDomain: 'test-6cf4b.firebaseapp.com',
+  projectId: 'test-6cf4b',
+  storageBucket: 'test-6cf4b.appspot.com',
+  messagingSenderId: '303312898998',
+  appId: '1:303312898998:web:0aa969b39faaa62ed6cf17',
+  measurementId: 'G-10V5YVPT58',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
