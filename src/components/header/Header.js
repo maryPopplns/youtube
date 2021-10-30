945915b09cfa9b11dfe8612f9a320b5c0f686ef9
Spencer Knight <spencer.knightusn@gmail.com>
2021-10-30 09:40:28 -0700

    header basics complete

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,14 @@
 import './header.css';
 import LeftHeader from './LeftHeader';
 import MiddleHeader from './MiddleHeader';
+import RightHeader from './RightHeader';
 
 export default function Header() {
   return (
     <header>
       <LeftHeader />
       <MiddleHeader />
+      <RightHeader />
     </header>
   );
 }
---
+++ src/components/header/RightHeader.js
@@ -0,0 +1,17 @@
+import './rightHeader.css';
+
+export default function RightHeader() {
+  return (
+    <div id='rightHeaderContainer'>
+      <div id='addVideoIconContainer'>
+        <svg viewBox='0 0 24 24'>
+          <path
+            d='M14,13h-3v3H9v-3H6v-2h3V8h2v3h3V13z M17,6H3v12h14v-6.39l4,1.83V8.56l-4,1.83V6 M18,5v3.83L22,7v8l-4-1.83V19H2V5H18L18,5 z'
+            fill='#FFFFFF'
+          ></path>
+        </svg>
+      </div>
+      <button>Sign In</button>
+    </div>
+  );
+}
