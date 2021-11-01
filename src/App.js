5c3c34339e2f26273c8f568d5406abd401759d22
Spencer Knight <spencer.knightusn@gmail.com>
2021-11-01 09:28:52 -0700

    basic routes complete

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import { initializeApp } from 'firebase/app';
 import Login from './components/header/Login';
 import Logout from './components/header/Logout';
-import defaultPofile from './assets/profile_placeholder.png';
 import YouTubeLogo from './components/header/YouTubeLogo';
+import SearchResults from './components/searchResults/SearchResults';
+import Upload from './components/upload/Upload';
+import Home from './components/home/Home';
 import firebaseConfig from './firebase-config';
+import defaultPofile from './assets/profile_placeholder.png';
 import './App.css';
 
 function App() {
@@ -36,14 +39,16 @@ function App() {
           </button>
         </form>
         <div id='rightHeaderContainer'>
-          <div id='addVideoIconContainer'>
-            <svg viewBox='0 0 24 24'>
-              <path
-                d='M14,13h-3v3H9v-3H6v-2h3V8h2v3h3V13z M17,6H3v12h14v-6.39l4,1.83V8.56l-4,1.83V6 M18,5v3.83L22,7v8l-4-1.83V19H2V5H18L18,5 z'
-                fill='#FFFFFF'
-              ></path>
-            </svg>
-          </div>
+          <Link to='/upload'>
+            <div id='addVideoIconContainer'>
+              <svg viewBox='0 0 24 24'>
+                <path
+                  d='M14,13h-3v3H9v-3H6v-2h3V8h2v3h3V13z M17,6H3v12h14v-6.39l4,1.83V8.56l-4,1.83V6 M18,5v3.83L22,7v8l-4-1.83V19H2V5H18L18,5 z'
+                  fill='#FFFFFF'
+                ></path>
+              </svg>
+            </div>
+          </Link>
           <img
             id='profilePicture'
             src={loggedIn ? getAuth().currentUser.photoURL : defaultPofile}
@@ -56,6 +61,17 @@ function App() {
           )}
         </div>
       </header>
+      <Switch>
+        <Route path='/searchResults'>
+          <SearchResults />
+        </Route>
+        <Route path='/upload'>
+          <Upload />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
