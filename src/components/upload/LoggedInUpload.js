d02e5e9d28b459bb2da34b5a85d997306a7b7317
Spencer Knight <spencer.knightusn@gmail.com>
2021-11-03 11:02:12 -0700

    added tags textarea to LoggedInUpload

--- src/components/upload/LoggedInUpload.js
+++ src/components/upload/LoggedInUpload.js
@@ -4,11 +4,13 @@ import './loggedInUpload.css';
 export default function LoggedInUpload() {
   const [videoTitle, setVideoTitle] = useState('');
   const [videoDescription, setVideoDescription] = useState('');
+  const [videoTags, setVideoTags] = useState('');
   // TODO create logic to upload videos
 
   const videoTitleHandler = (event) => setVideoTitle(event.target.value);
   const videoDescriptionHandler = (event) =>
     setVideoDescription(event.target.value);
+  const videoTagsHandler = (event) => setVideoTags(event.target.value);
 
   return (
     <main id='loggedInUploadContainer'>
@@ -17,21 +19,34 @@ export default function LoggedInUpload() {
         <div className='inputContainer'>
           <label for='videoTitle'>Title: </label>
           <input
+            className='uploadVideoInput'
             type='text'
             id='videoTitle'
             name='videoTitle'
             value={videoTitle}
             onChange={videoTitleHandler}
+            required
           ></input>
         </div>
         <div className='inputContainer'>
           <label for='videoDescription'>Description: </label>
           <textarea
-            cols='15'
+            className='uploadVideoInput'
             id='videoDescription'
             name='videoDescription'
             value={videoDescription}
             onChange={videoDescriptionHandler}
+            required
+          ></textarea>
+        </div>
+        <div className='inputContainer'>
+          <label for='videoTags'>Tags: </label>
+          <textarea
+            className='uploadVideoInput'
+            id='videoTags'
+            name='videoTags'
+            value={videoTags}
+            onChange={videoTagsHandler}
           ></textarea>
         </div>
       </form>
