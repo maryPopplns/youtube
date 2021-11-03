5214bf58f29f0c024c10c48118c589cb87cf72e4
Spencer Knight <spencer.knightusn@gmail.com>
2021-11-03 10:38:12 -0700

    added textArea field for video description

--- src/components/upload/LoggedInUpload.js
+++ src/components/upload/LoggedInUpload.js
@@ -3,22 +3,37 @@ import './loggedInUpload.css';
 
 export default function LoggedInUpload() {
   const [videoTitle, setVideoTitle] = useState('');
+  const [videoDescription, setVideoDescription] = useState('');
   // TODO create logic to upload videos
-  function videoTitleHandler(event) {
-    setVideoTitle(event.target.value);
-  }
+
+  const videoTitleHandler = (event) => setVideoTitle(event.target.value);
+  const videoDescriptionHandler = (event) =>
+    setVideoDescription(event.target.value);
 
   return (
     <main id='loggedInUploadContainer'>
       <h2 id='loggedInUploadHeading'>Upload Video</h2>
-      <form>
-        <label for='videoTitle'>Title: </label>
-        <input
-          id='videoTitle'
-          name='videoTitle'
-          value={videoTitle}
-          onChange={videoTitleHandler}
-        ></input>
+      <form id='uploadVideoForm'>
+        <div className='inputContainer'>
+          <label for='videoTitle'>Title: </label>
+          <input
+            type='text'
+            id='videoTitle'
+            name='videoTitle'
+            value={videoTitle}
+            onChange={videoTitleHandler}
+          ></input>
+        </div>
+        <div className='inputContainer'>
+          <label for='videoDescription'>Description: </label>
+          <textarea
+            cols='15'
+            id='videoDescription'
+            name='videoDescription'
+            value={videoDescription}
+            onChange={videoDescriptionHandler}
+          ></textarea>
+        </div>
       </form>
     </main>
   );
