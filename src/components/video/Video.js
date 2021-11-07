98e26d0b889631d5515676888aa5252bf423b9ae
Spencer Knight <spencer.knightusn@gmail.com>
2021-11-06 19:37:29 -0700

    home page completet

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,9 +1,17 @@
 import './video.css';
+import ReactPlayer from 'react-player/lazy';
 
 export default function Video(props) {
   return (
     <div id='videoPlayer'>
-      <div id='videoContainer'></div>
+      <div id='videoContainer'>
+        <video
+          width='640'
+          height='480'
+          src={props.currentVideo.videoPath}
+          controls
+        />
+      </div>
     </div>
   );
 }
