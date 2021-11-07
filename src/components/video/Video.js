611993270a8472946b64c7042841bca3694de03d
Spencer Knight <spencer.knightusn@gmail.com>
2021-11-06 18:57:12 -0700

    added react-player

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,6 +1,9 @@
 import './video.css';
 
 export default function Video(props) {
-  console.log(props.currentVideo);
-  return <div id='videoPlayer'>hello!</div>;
+  return (
+    <div id='videoPlayer'>
+      <div id='videoContainer'></div>
+    </div>
+  );
 }
